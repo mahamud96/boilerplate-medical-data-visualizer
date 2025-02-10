e6ae545ec0a8eb2aac9acdaadb281dddabe09486
Mahamud Ahmed <mahamud1996@outlook.com>
2025-02-10 22:02:15 +0000

    sdasd

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from unittest import main
 medical_data_visualizer.draw_cat_plot()
 medical_data_visualizer.draw_heat_map()
 
+
 # Run unit tests automatically
 main(module='test_module', exit=False)
